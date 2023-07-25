620ac445d319dffd3e7bdccb4f777eff29e55db2
Zhiyu Wu <zhiyuwu@ampere02.eecs.umich.edu>
2023-07-25 01:43:16 -0400

    rm llama 2 prompt

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -37,13 +37,6 @@ SYSTEM_PROMPTS = {
         "Write a response that appropriately completes the request. "
         "The response should be very concise. "
     ),
-    "llama-2": (
-        "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. "
-        "Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. "
-        "Please ensure that your responses are socially unbiased and positive in nature.\n\n"
-        "If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. "
-        "If you don't know the answer to a question, please don't share false information."
-    ),
 }
 
 def is_partial_stop(output: str, stop_str: str):
