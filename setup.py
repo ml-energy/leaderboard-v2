46f056c6312abf4d6936a5a9bc8c4653813bb884
Jae-Won Chung <jwnchung@umich.edu>
2023-08-07 01:03:37 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ setup(
     url="https://github.com/ml-energy/leaderboard",
     packages=find_packages("."),
     extras_require={
-        "colosseum-controller": ["fastapi", "fschat==0.2.20"],
+        # TODO: text_generation actually means our internal fork.
+        #       One way to go about this is to open source only the python client
+        #       in a separate repo and then use that as a dependency here (git+https).
+        "colosseum-controller": ["fastapi", "fschat==0.2.20", "./text_generation"],
         "app": ["plotly==5.15.0", "gradio==3.39.0", "pydantic==1.10.9"],
         "benchmark": ["zeus-ml", "fschat==0.2.20", "tyro", "rich"],
     }
