c604c7271da4760e92fac7139ad94a7fa34fddb7
Jae-Won Chung <jwnchung@umich.edu>
2023-08-08 20:41:59 -0400

    Tidy up ControllerClient

--- spitfight/colosseum/client.py
+++ spitfight/colosseum/client.py
@@ -9,7 +9,6 @@ from typing import Generator, Literal
 
 import requests
 import gradio as gr
-from pydantic import BaseModel
 
 from spitfight.colosseum.common import (
     COLOSSEUM_PROMPT_ROUTE,
@@ -33,10 +32,7 @@ class ControllerClient:
         self.request_id = str(request_id) or str(uuid4())
 
     def fork(self) -> ControllerClient:
-        """Return a copy of the client with a new request ID.
-
-        This exploints the fact that gr.State simply deepcopies objects.
-        """
+        """Return a copy of the client with a new request ID."""
         return ControllerClient(
             controller_addr=self.controller_addr,
             timeout=self.timeout,
@@ -53,7 +49,7 @@ class ControllerClient:
                 stream=True,
                 timeout=self.timeout,
             )
-        _check_response(prompt_request, resp)
+        _check_response(resp)
         # XXX: Why can't the server just yield `text + "\n"` and here we just iter_lines?
         for chunk in resp.iter_lines(decode_unicode=False, delimiter=b"\0"):
             if chunk:
@@ -62,21 +58,23 @@ class ControllerClient:
     def response_vote(self, victory_index: Literal[0, 1]) -> ResponseVoteResponse:
         """Notify the controller of the user's vote for the response."""
         response_vote_request = ResponseVoteRequest(request_id=self.request_id, victory_index=victory_index)
-        resp = requests.post(
-            f"http://{self.controller_addr}{COLOSSEUM_RESP_VOTE_ROUTE}",
-            json=response_vote_request.dict(),
-        )
-        _check_response(response_vote_request, resp)
+        with _catch_requests_exceptions():
+            resp = requests.post(
+                f"http://{self.controller_addr}{COLOSSEUM_RESP_VOTE_ROUTE}",
+                json=response_vote_request.dict(),
+            )
+        _check_response(resp)
         return ResponseVoteResponse(**resp.json())
 
     def energy_vote(self, is_worth: bool) -> EnergyVoteResponse:
         """Notify the controller of the user's vote for energy."""
         energy_vote_request = EnergyVoteRequest(request_id=self.request_id, is_worth=is_worth)
-        resp = requests.post(
-            f"http://{self.controller_addr}{COLOSSEUM_ENERGY_VOTE_ROUTE}",
-            json=energy_vote_request.dict(),
-        )
-        _check_response(energy_vote_request, resp)
+        with _catch_requests_exceptions():
+            resp = requests.post(
+                f"http://{self.controller_addr}{COLOSSEUM_ENERGY_VOTE_ROUTE}",
+                json=energy_vote_request.dict(),
+            )
+        _check_response(resp)
         return EnergyVoteResponse(**resp.json())
 
 
@@ -89,7 +87,7 @@ def _catch_requests_exceptions():
         raise gr.Error("Failed to connect to our the backend server. Please try again later.")
 
 
-def _check_response(request: BaseModel, response: requests.Response) -> None:
+def _check_response(response: requests.Response) -> None:
     if 400 <= response.status_code < 500:
         raise gr.Error(response.json()["detail"])
     elif response.status_code >= 500:
