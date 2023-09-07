0bf074c66da43e3216757cff356a961c8f779f31
Zhiyu Wu <julius@JuliusWu>
2023-09-07 06:08:22 -0400

    more exact num to ckeck OOM

--- scripts/compute_system_metrics.py
+++ scripts/compute_system_metrics.py
@@ -21,7 +21,9 @@ def main(data_dir: str, out_file: str) -> None:
         for benchmark_file in glob(f"{data_dir}/{model_name}/benchmark_batch_*.json"):
             batch_size = int(benchmark_file.split("_")[-1][:-5])
             df = pd.read_json(benchmark_file)
-            if len(df) < 100:
+            if len(df) < 2978 // batch_size and "Llama-2-7b" not in model_name:
+                out_csv.writerow([model_name.replace("--", "/"), str(batch_size)] + ["OOM"])
+            elif len(df) < (2978 // batch_size)*0.9 and "Llama-2-7b" in model_name: 
                 out_csv.writerow([model_name.replace("--", "/"), str(batch_size)] + ["OOM"])
             else:
                 out_csv.writerow(
