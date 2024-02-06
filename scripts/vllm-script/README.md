db78f2b4dc0b7ba2cf00625087e24fd9bbddd212
Oh Jun Kweon <ohjunjps@gmail.com>
2024-02-06 01:27:42 -0500

    feat: benchmark tgi integration (currently blocking)
    Benchmark script correctly reports energy consumption using TGI. However, the script currently blocks between each request, resulting in batch sizes of 1 only. Currently unsure how I can make the requests be sent without blocking while keeping track of the energy reported in the response. Will look into this further tomorrow.

---
+++ scripts/vllm-script/README.md
@@ -0,0 +1,61 @@
+WIP: temporarily storing some common commands here
+
+# run tgi server
+```
+token=<TOKEN>
+docker run --gpus all --shm-size 1g -p 8000:80 \
+        -e HUGGING_FACE_HUB_TOKEN=$token \
+        -v $PWD/Llama-2-7b-chat-hf-tokenizer_config.json:/app/tokenizer_config.json \
+        --name tgi_v1.4.0 \
+        tgi_v1.4.0 \
+        --tokenizer-config-path /app/tokenizer_config.json \
+        --model-id meta-llama/Llama-2-7b-chat-hf
+```
+
+# client side: run benchmark script
+```
+python benchmark_server.py \
+        --backend tgi \
+        --dataset ../../sharegpt/ShareGPT_V3_filtered.json \
+        --model meta-llama/Llama-2-7b-hf \
+        --host 127.0.0.1 \
+        --num-prompts 2  # default=1000
+```
+
+# client side: docker way (need to use docker network here and on backend)
+Dockerfile:
+```
+# Use an official Python runtime as a parent image
+FROM python:3.9
+
+# Set the working directory in the container
+WORKDIR /benchmark
+
+# Copy the current directory contents into the container at /benchmark
+COPY . .
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Run script.py when the container launches
+ENTRYPOINT ["python", "benchmark_server.py"]
+```
+CLI:
+```
+docker build -t benchmark:latest .
+docker run \
+        -v /home/ohjun/leaderboard/sharegpt:/data \
+        benchmark:latest \
+        --backend tgi \
+        --dataset /data/ShareGPT_V3_filtered.json \
+        --model meta-llama/Llama-2-7b-hf \
+```
+
+# requirements.txt
+```
+argparse
+asyncio
+aiohttp
+numpy
+tqdm
+```
--- scripts/vllm-script/benchmark_server.py
+++ scripts/vllm-script/benchmark_server.py
@@ -2,22 +2,10 @@
    Difference with benchmark.py: integration with model hosted on dedicated inference server.
    Currently supports:
    - Hugging Face TGI (Text Generation Inference)
-
-On the server side, run one of the following commands:
-    (vLLM backend)
-    python -m vllm.entrypoints.api_server \
-        --model <your_model> --swap-space 16 \
-        --disable-log-requests
-
-    (TGI backend)
-    ./launch_hf_server.sh <your_model>
-
-On the client side, run:
-    python benchmarks/benchmark_serving.py \
-        --backend <backend> \
-        --tokenizer <your_model> --dataset <target_dataset> \
-        --request-rate <request_rate>
+   TODO:
+   - vLLM
 """
+
 import argparse
 import asyncio
 import json
@@ -28,60 +16,32 @@ from typing import AsyncGenerator, List, Tuple
 import aiohttp
 import numpy as np
 from tqdm.asyncio import tqdm
-from transformers import PreTrainedTokenizerBase
-from vllm.transformers_utils.tokenizer import get_tokenizer
 
 # (prompt len, output len, latency)
 REQUEST_LATENCY: List[Tuple[int, int, float]] = []
+SYSTEM_PROMPT = "A chat between a human user (prompter) and an artificial intelligence (AI) assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. "
+TOTAL_ENERGY = 0
+TOTAL_PROMPT_TOKENS = 0
+TOTAL_COMPLETION_TOKENS = 0
 
 
 def sample_requests(
     dataset_path: str,
     num_requests: int,
-    tokenizer: PreTrainedTokenizerBase,
-) -> List[Tuple[str, int, int]]:
+) -> List[str]:
     # Load the dataset.
     with open(dataset_path) as f:
         dataset = json.load(f)
-    # Filter out the conversations with less than 2 turns.
-    dataset = [data for data in dataset if len(data["conversations"]) >= 2]
     # Only keep the first two turns of each conversation.
-    dataset = [
-        (data["conversations"][0]["value"], data["conversations"][1]["value"])
-        for data in dataset
-    ]
-
-    # Tokenize the prompts and completions.
-    prompts = [prompt for prompt, _ in dataset]
-    prompt_token_ids = tokenizer(prompts).input_ids
-    completions = [completion for _, completion in dataset]
-    completion_token_ids = tokenizer(completions).input_ids
-    tokenized_dataset = []
-    for i in range(len(dataset)):
-        output_len = len(completion_token_ids[i])
-        tokenized_dataset.append((prompts[i], prompt_token_ids[i], output_len))
-
-    # Filter out too long sequences.
-    filtered_dataset: List[Tuple[str, int, int]] = []
-    for prompt, prompt_token_ids, output_len in tokenized_dataset:
-        prompt_len = len(prompt_token_ids)
-        if prompt_len < 4 or output_len < 4:
-            # Prune too short sequences.
-            # This is because TGI causes errors when the input or output length
-            # is too short.
-            continue
-        if prompt_len > 1024 or prompt_len + output_len > 2048:
-            # Prune too long sequences.
-            continue
-        filtered_dataset.append((prompt, prompt_len, output_len))
+    dataset = [data["conversations"][0]["value"] for data in dataset]
 
     # Sample the requests.
-    sampled_requests = random.sample(filtered_dataset, num_requests)
+    sampled_requests = random.sample(dataset, num_requests)
     return sampled_requests
 
 
 async def get_request(
-    input_requests: List[Tuple[str, int, int]],
+    input_requests: List[str],
     request_rate: float,
 ) -> AsyncGenerator[Tuple[str, int, int], None]:
     input_requests = iter(input_requests)
@@ -97,36 +57,27 @@ async def get_request(
         await asyncio.sleep(interval)
 
 
-async def send_request(backend: str, model: str, api_url: str, prompt: str,
-                       prompt_len: int, output_len: int, best_of: int,
-                       use_beam_search: bool, pbar: tqdm) -> None:
+async def send_request(
+    backend: str,
+    model: str,
+    api_url: str,
+    prompt: str,
+    pbar: tqdm,
+) -> None:
     request_start_time = time.perf_counter()
 
-    headers = {"User-Agent": "Benchmark Client"}
-    if backend == "vllm":
+    # headers = {"User-Agent": "Benchmark Client"}
+    headers = {"Content-Type": "application/json"}
+    # Both tgi and vllm support OpenAI Chat Completion API
+    if backend == "tgi" or "vllm":
         pload = {
-            "prompt": prompt,
-            "n": 1,
-            "best_of": best_of,
-            "use_beam_search": use_beam_search,
-            "temperature": 0.0 if use_beam_search else 1.0,
-            "top_p": 1.0,
-            "max_tokens": output_len, # TODO 2000
-            "ignore_eos": True,
+            "model": model,
+            "messages": [
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": prompt},
+            ],
             "stream": False,
-        }
-        if model is not None:
-            pload["model"] = model
-    elif backend == "tgi":
-        assert not use_beam_search
-        params = {
-            "best_of": best_of,
-            "max_new_tokens": output_len,
-            "do_sample": True,
-        }
-        pload = {
-            "inputs": prompt,
-            "parameters": params,
+            "max_tokens": 1000,  # tgi: `inputs` tokens + `max_new_tokens` must be <= 2048
         }
     else:
         raise ValueError(f"Unknown backend: {backend}")
@@ -134,8 +85,7 @@ async def send_request(backend: str, model: str, api_url: str, prompt: str,
     timeout = aiohttp.ClientTimeout(total=3 * 3600)
     async with aiohttp.ClientSession(timeout=timeout) as session:
         while True:
-            async with session.post(api_url, headers=headers,
-                                    json=pload) as response:
+            async with session.post(api_url, headers=headers, json=pload) as response:
                 chunks = []
                 async for chunk, _ in response.content.iter_chunks():
                     chunks.append(chunk)
@@ -148,7 +98,25 @@ async def send_request(backend: str, model: str, api_url: str, prompt: str,
 
     request_end_time = time.perf_counter()
     request_latency = request_end_time - request_start_time
-    REQUEST_LATENCY.append((prompt_len, output_len, request_latency))
+    # TODO: understand why "global REQUEST_LATENCY" isn't required for this
+    #       same with SYSTEM_PROMPT above
+    #       TOTAL_ENERGY, etc. says "local variable 'TOTAL_ENERGY' referenced before assignment" without declaring global
+    REQUEST_LATENCY.append(
+        (
+            output["usage"]["prompt_tokens"],
+            output["usage"]["completion_tokens"],
+            request_latency,
+        )
+    )
+
+    # track energy usage
+    global TOTAL_ENERGY
+    global TOTAL_PROMPT_TOKENS
+    global TOTAL_COMPLETION_TOKENS
+    TOTAL_ENERGY += output["usage"]["energy"]
+    TOTAL_PROMPT_TOKENS += output["usage"]["prompt_tokens"]
+    TOTAL_COMPLETION_TOKENS += output["usage"]["completion_tokens"]
+
     pbar.update(1)
 
 
@@ -156,18 +124,14 @@ async def benchmark(
     backend: str,
     model: str,
     api_url: str,
-    input_requests: List[Tuple[str, int, int]],
-    best_of: int,
-    use_beam_search: bool,
+    input_requests: List[str],
     request_rate: float,
 ) -> None:
     tasks: List[asyncio.Task] = []
     pbar = tqdm(total=len(input_requests))
     async for request in get_request(input_requests, request_rate):
-        prompt, prompt_len, output_len = request
-        task = asyncio.create_task(
-            send_request(backend, model, api_url, prompt, prompt_len,
-                         output_len, best_of, use_beam_search, pbar))
+        prompt = request
+        task = asyncio.create_task(send_request(backend, model, api_url, prompt, pbar))
         tasks.append(task)
     await asyncio.gather(*tasks)
     pbar.close()
@@ -179,76 +143,87 @@ def main(args: argparse.Namespace):
     np.random.seed(args.seed)
 
     api_url = f"{args.protocol}://{args.host}:{args.port}{args.endpoint}"
-    tokenizer = get_tokenizer(args.tokenizer,
-                              trust_remote_code=args.trust_remote_code)
-    input_requests = sample_requests(args.dataset, args.num_prompts, tokenizer)
+    input_requests = sample_requests(args.dataset, args.num_prompts)
 
     benchmark_start_time = time.perf_counter()
     asyncio.run(
-        benchmark(args.backend, args.model, api_url, input_requests,
-                  args.best_of, args.use_beam_search, args.request_rate))
+        benchmark(
+            args.backend,
+            args.model,
+            api_url,
+            input_requests,
+            args.request_rate,
+        )
+    )
     benchmark_end_time = time.perf_counter()
     benchmark_time = benchmark_end_time - benchmark_start_time
     print(f"Total time: {benchmark_time:.2f} s")
     print(f"Throughput: {args.num_prompts / benchmark_time:.2f} requests/s")
+    print(f"Total energy: {TOTAL_ENERGY:.2f} J")
 
-    # Compute the latency statistics.
+    # Compute the latency statistics
     avg_latency = np.mean([latency for _, _, latency in REQUEST_LATENCY])
     print(f"Average latency: {avg_latency:.2f} s")
-    avg_per_token_latency = np.mean([
-        latency / (prompt_len + output_len)
-        for prompt_len, output_len, latency in REQUEST_LATENCY
-    ])
+    avg_per_token_latency = np.mean(
+        [
+            latency / (prompt_len + output_len)
+            for prompt_len, output_len, latency in REQUEST_LATENCY
+        ]
+    )
     print(f"Average latency per token: {avg_per_token_latency:.2f} s")
     avg_per_output_token_latency = np.mean(
-        [latency / output_len for _, output_len, latency in REQUEST_LATENCY])
-    print("Average latency per output token: "
-          f"{avg_per_output_token_latency:.2f} s")
+        [latency / output_len for _, output_len, latency in REQUEST_LATENCY]
+    )
+    print("Average latency per output token: " f"{avg_per_output_token_latency:.2f} s")
+
+    # Compute the energy statistics
+    print(f"Total prompt tokens: {TOTAL_PROMPT_TOKENS}")
+    print(f"Total completion tokens: {TOTAL_COMPLETION_TOKENS}")
+    energy_per_token = TOTAL_ENERGY / (
+        TOTAL_PROMPT_TOKENS + TOTAL_COMPLETION_TOKENS
+    )  # TODO: should this divide only by TOTAL_COMPLETION_TOKENS?
+    print(f"Energy per token: {energy_per_token:.2f} J")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Benchmark the online serving throughput.")
-    parser.add_argument("--backend",
-                        type=str,
-                        default="vllm",
-                        choices=["vllm", "tgi"])
-    parser.add_argument("--protocol",
-                        type=str,
-                        default="http",
-                        choices=["http", "https"])
+        description="Benchmark the online serving throughput."
+    )
+    parser.add_argument("--backend", type=str, default="vllm", choices=["vllm", "tgi"])
+    parser.add_argument(
+        "--protocol", type=str, default="http", choices=["http", "https"]
+    )
     parser.add_argument("--host", type=str, default="localhost")
     parser.add_argument("--port", type=int, default=8000)
-    parser.add_argument("--endpoint", type=str, default="/generate")
+    parser.add_argument("--endpoint", type=str, default="/v1/chat/completions")
     parser.add_argument("--model", type=str, default=None)
-    parser.add_argument("--dataset",
-                        type=str,
-                        required=True,
-                        help="Path to the dataset.")
-    parser.add_argument("--tokenizer",
-                        type=str,
-                        required=True,
-                        help="Name or path of the tokenizer.")
-    parser.add_argument("--best-of",
-                        type=int,
-                        default=1,
-                        help="Generates `best_of` sequences per prompt and "
-                        "returns the best one.")
-    parser.add_argument("--use-beam-search", action="store_true")
-    parser.add_argument("--num-prompts",
-                        type=int,
-                        default=1000,
-                        help="Number of prompts to process.")
-    parser.add_argument("--request-rate",
-                        type=float,
-                        default=float("inf"),
-                        help="Number of requests per second. If this is inf, "
-                        "then all the requests are sent at time 0. "
-                        "Otherwise, we use Poisson process to synthesize "
-                        "the request arrival times.")
+    parser.add_argument(
+        "--dataset", type=str, required=True, help="Path to the dataset."
+    )
+    # parser.add_argument(
+    #     "--best-of",
+    #     type=int,
+    #     default=1,
+    #     help="Generates `best_of` sequences per prompt and " "returns the best one.",
+    # )
+    # parser.add_argument("--use-beam-search", action="store_true")
+    parser.add_argument(
+        "--num-prompts", type=int, default=1000, help="Number of prompts to process."
+    )
+    parser.add_argument(
+        "--request-rate",
+        type=float,
+        default=float("inf"),
+        help="Number of requests per second. If this is inf, "
+        "then all the requests are sent at time 0. "
+        "Otherwise, we use Poisson process to synthesize "
+        "the request arrival times.",
+    )
     parser.add_argument("--seed", type=int, default=0)
-    parser.add_argument('--trust-remote-code',
-                        action='store_true',
-                        help='trust remote code from huggingface')
+    parser.add_argument(
+        "--trust-remote-code",
+        action="store_true",
+        help="trust remote code from huggingface",
+    )
     args = parser.parse_args()
     main(args)
